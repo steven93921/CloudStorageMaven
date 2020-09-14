334e1576dd95951b7ef83f510b97ada756c51c31
Steven Dillingham <steven.dillingham@gmail.com>
2020-09-14 13:04:12 -0400

    Extend the S3Connect utility to allow configuration of the connection parameters including support for credentials (including a profile).

--- S3StorageWagon/src/main/java/com/gkatzioura/maven/cloud/s3/utils/S3Connect.java
+++ S3StorageWagon/src/main/java/com/gkatzioura/maven/cloud/s3/utils/S3Connect.java
@@ -10,7 +10,13 @@ package com.gkatzioura.maven.cloud.s3.utils;
 
 import com.amazonaws.SdkClientException;
 import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.S3ClientOptions;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.gkatzioura.maven.cloud.s3.CredentialsFactory;
 import com.gkatzioura.maven.cloud.s3.EndpointProperty;
@@ -31,6 +37,40 @@ public class S3Connect {
      */
     private static final Logger LOGGER = Logger.getLogger(S3Connect.class.getName());
 
+    private S3StorageRegionProviderChain regionProvider = null;
+    private EndpointProperty endpoint = EndpointProperty.empty();
+    private AWSCredentialsProvider credentialsProvider = new DefaultAWSCredentialsProviderChain();
+    private PathStyleEnabledProperty pathStyle = new PathStyleEnabledProperty(String.valueOf(S3ClientOptions.DEFAULT_PATH_STYLE_ACCESS));
+
+    public S3Connect(String region) {
+        regionProvider = new S3StorageRegionProviderChain(region);
+    }
+
+    public S3Connect(EndpointProperty endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    public void setCredentials(String accessKeyId, String secretKey) {
+        setCredentialsProvider(new BasicAWSCredentials(accessKeyId, secretKey));
+    }
+
+    public void setCredentialsProfile(String profile) {
+        setCredentialsProvider(new ProfileCredentialsProvider(profile));
+    }
+
+    public void setCredentialsProvider(BasicAWSCredentials credentials) {
+        credentialsProvider = new AWSStaticCredentialsProvider(credentials);
+    }
+
+    public void setCredentialsProvider(ProfileCredentialsProvider profileCredentialsProvider) {
+        credentialsProvider = profileCredentialsProvider;
+    }
+
+    public void setPathStyleEnabled(PathStyleEnabledProperty pathStyle) {
+        this.pathStyle = pathStyle;
+    }
+
+
     /**
      * Connects to the AWS API. The provided authentication, region, endpoint and path-style are all taken into account
      * to create the returned {@link AmazonS3} instance.
@@ -49,17 +89,42 @@ public class S3Connect {
      * @throws AuthenticationException if the passed credentials are invalid for connecting to the intended endpoint/bucket.
      */
     public static AmazonS3 connect(AuthenticationInfo authenticationInfo, String region, EndpointProperty endpoint, PathStyleEnabledProperty pathStyle) throws AuthenticationException {
-        AmazonS3ClientBuilder builder = null;
+
+        S3Connect s3Connect = endpoint.isPresent() ? new S3Connect(endpoint) : new S3Connect(region);
+
+        if (authenticationInfo != null) {
+            s3Connect.setCredentials(authenticationInfo.getUserName(), authenticationInfo.getPassword());
+        }
+
+        s3Connect.setPathStyleEnabled(pathStyle);
+
+        return s3Connect.connect();     
+    }
+
+    /**
+     * Connects to the AWS API. Any provided credentials, profile, region, endpoint and path-style are all taken into account
+     * to create the returned {@link AmazonS3} instance.
+     *
+     * @return An instance of {@link AmazonS3} that can be used to send and receive data to the intended endpoint/bucket.
+     * @throws AuthenticationException if the credentials are invalid for connecting to the intended endpoint/bucket.
+     */
+    public AmazonS3 connect() throws AuthenticationException {
+        AmazonS3ClientBuilder builder = AmazonS3ClientBuilder.standard().withCredentials(credentialsProvider);
+
+        if (endpoint.isPresent()){
+            builder.setEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint.get(), builder.getRegion()));
+        } else {
+            builder.setRegion(regionProvider.getRegion());
+        }
+
+        builder.setPathStyleAccessEnabled(pathStyle.get());      
+
         try {
-            builder = createAmazonS3ClientBuilder(authenticationInfo, region, endpoint, pathStyle);
-
             AmazonS3 amazonS3 = builder.build();
-
             LOGGER.finer(String.format("Connected to S3 using bucket %s.", endpoint.get()));
-
             return amazonS3;
         } catch (SdkClientException e) {
-            if (builder != null){
+            if (builder != null) {
                 StringBuilder message = new StringBuilder();
                 message.append("Failed to connect");
                 if (builder.getEndpoint() != null){
@@ -74,22 +139,6 @@ public class S3Connect {
                 throw new AuthenticationException(message.toString(), e);
             }
             throw new AuthenticationException("Could not authenticate", e);
-        }
-    }
-
-    private static AmazonS3ClientBuilder createAmazonS3ClientBuilder(AuthenticationInfo authenticationInfo, String region, EndpointProperty endpoint, PathStyleEnabledProperty pathStyle) {
-        final S3StorageRegionProviderChain regionProvider = new S3StorageRegionProviderChain(region);
-
-        AmazonS3ClientBuilder builder;
-        builder = AmazonS3ClientBuilder.standard().withCredentials(new CredentialsFactory().create(authenticationInfo));
-
-        if (endpoint.isPresent()){
-            builder.setEndpointConfiguration( new AwsClientBuilder.EndpointConfiguration(endpoint.get(), builder.getRegion()));
-        } else {
-            builder.setRegion(regionProvider.getRegion());
-        }
-
-        builder.setPathStyleAccessEnabled(pathStyle.get());
-        return builder;
+        }          
     }
 }
